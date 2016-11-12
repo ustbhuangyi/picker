b4b0a4f04776ae5cfb9332469fdb27fcda87cfa0
ustbhuangyi <280309453@qq.com>
2016-11-12 23:13:37 +0800

    add  refillColumn、scrollColumn api

--- demo/index.js
+++ demo/index.js
@@ -1,17 +1,21 @@
 var nameEl = document.getElementById('chose');
 
-var first = []; /* 省，直辖市 */
-var second = []; /* 市 */
-var third = []; /* 镇 */
+var first = [];
+/* 省，直辖市 */
+var second = [];
+/* 市 */
+var third = [];
+/* 镇 */
 
-var checked = [0, 0, 0]; /* 已选选项 */
+var checked = [0, 0, 0];
+/* 已选选项 */
 
-function creatList(obj, list){
-  obj.forEach(function(item, index, arr){
-  var temp = new Object();
-  temp.text = item.name;
-  temp.value = index;
-  list.push(temp);
+function creatList(obj, list) {
+  obj.forEach(function (item, index, arr) {
+    var temp = new Object();
+    temp.text = item.name;
+    temp.value = index;
+    list.push(temp);
   })
 }
 
@@ -30,9 +34,9 @@ if (city[0].sub[0].hasOwnProperty('sub')) {
 }
 
 var picker = new Picker({
-	data: [first, second, third],
-	selectedIndex: [0, 0, 0],
-	title: '地址选择'
+  data: [first, second, third],
+  selectedIndex: [0, 0, 0],
+  title: '地址选择'
 });
 
 picker.on('picker.select', function (selectedVal, selectedIndex) {
@@ -40,11 +44,11 @@ picker.on('picker.select', function (selectedVal, selectedIndex) {
   var text2 = second[selectedIndex[1]].text;
   var text3 = third[selectedIndex[2]] ? third[selectedIndex[2]].text : '';
 
-	nameEl.innerText = text1 + ' ' + text2 + ' ' + text3;
+  nameEl.innerText = text1 + ' ' + text2 + ' ' + text3;
 });
 
 picker.on('picker.change', function (index, selectedIndex) {
-  if (index === 0){
+  if (index === 0) {
     firstChange();
   } else if (index === 1) {
     secondChange();
@@ -63,7 +67,7 @@ picker.on('picker.change', function (index, selectedIndex) {
       checked[1] = 0;
       checked[2] = 0;
     }
-    
+
     var secondCity = city[selectedIndex].sub[0]
     if (secondCity.hasOwnProperty('sub')) {
       creatList(secondCity.sub, third);
@@ -71,8 +75,8 @@ picker.on('picker.change', function (index, selectedIndex) {
       third = [{text: '', value: 0}];
       checked[2] = 0;
     }
-    picker.refill(second, 1);
-    picker.refill(third, 2);
+    picker.refillColumn(1, second);
+    picker.refillColumn(2, third);
   }
 
   function secondChange() {
@@ -82,11 +86,11 @@ picker.on('picker.change', function (index, selectedIndex) {
     if (city[first_index].sub[selectedIndex].hasOwnProperty('sub')) {
       var secondCity = city[first_index].sub[selectedIndex];
       creatList(secondCity.sub, third);
-      picker.refill(third, 2);
+      picker.refillColumn(2, third);
     } else {
       third = [{text: '', value: 0}];
       checked[2] = 0;
-      picker.refill(third, 2);
+      picker.refillColumn(2, third);
     }
   }
 
@@ -98,7 +102,7 @@ picker.on('picker.valuechange', function (selectedVal, selectedIndex) {
 });
 
 nameEl.addEventListener('click', function () {
-	picker.show();
+  picker.show();
 });
 
 
--- src/picker/picker.js
+++ src/picker/picker.js
@@ -2,168 +2,184 @@ import BScroll from 'better-scroll';
 import EventEmitter from '../util/eventEmitter';
 import {extend} from '../util/lang';
 import {
-	createDom,
-	addEvent,
-	addClass,
-	removeClass
+  createDom,
+  addEvent,
+  addClass,
+  removeClass
 } from '../util/dom';
 import pickerTemplate from './picker.handlebars';
 import itemTemplate from './item.handlebars';
 import './picker.styl';
 
 export default class Picker extends EventEmitter {
-	constructor(options) {
-		super();
+  constructor(options) {
+    super();
 
-		this.options = {
-			data: [],
-			title: '',
-			selectedIndex: null,
-			showCls: 'show'
-		};
+    this.options = {
+      data: [],
+      title: '',
+      selectedIndex: null,
+      showCls: 'show'
+    };
 
-		extend(this.options, options);
+    extend(this.options, options);
 
-		this.data = this.options.data;
-		this.pickerEl = createDom(pickerTemplate({
-			data: this.data,
-			title: this.options.title
-		}));
+    this.data = this.options.data;
+    this.pickerEl = createDom(pickerTemplate({
+      data: this.data,
+      title: this.options.title
+    }));
 
-		document.body.appendChild(this.pickerEl);
+    document.body.appendChild(this.pickerEl);
 
-		this.maskEl = this.pickerEl.getElementsByClassName('mask-hook')[0];
-		this.wheelEl = this.pickerEl.getElementsByClassName('wheel-hook');
-		this.panelEl = this.pickerEl.getElementsByClassName('panel-hook')[0];
-		this.confirmEl = this.pickerEl.getElementsByClassName('confirm-hook')[0];
-		this.cancelEl = this.pickerEl.getElementsByClassName('cancel-hook')[0];
-		this.scrollEl = this.pickerEl.getElementsByClassName('wheel-scroll-hook');
+    this.maskEl = this.pickerEl.getElementsByClassName('mask-hook')[0];
+    this.wheelEl = this.pickerEl.getElementsByClassName('wheel-hook');
+    this.panelEl = this.pickerEl.getElementsByClassName('panel-hook')[0];
+    this.confirmEl = this.pickerEl.getElementsByClassName('confirm-hook')[0];
+    this.cancelEl = this.pickerEl.getElementsByClassName('cancel-hook')[0];
+    this.scrollEl = this.pickerEl.getElementsByClassName('wheel-scroll-hook');
 
-		this._init();
-	}
+    this._init();
+  }
 
-	_init() {
-		this.selectedIndex = [];
-		this.selectedVal = [];
-		if (this.options.selectedIndex) {
-			this.selectedIndex = this.options.selectedIndex;
-		} else {
-			for (let i = 0; i < this.data.length; i++) {
-				this.selectedIndex[i] = 0;
-			}
-		}
+  _init() {
+    this.selectedIndex = [];
+    this.selectedVal = [];
+    if (this.options.selectedIndex) {
+      this.selectedIndex = this.options.selectedIndex;
+    } else {
+      for (let i = 0; i < this.data.length; i++) {
+        this.selectedIndex[i] = 0;
+      }
+    }
 
-		this._bindEvent();
-	}
+    this._bindEvent();
+  }
 
-	_bindEvent() {
-		addEvent(this.pickerEl, 'touchmove', (e) => {
-			e.preventDefault();
-		});
+  _bindEvent() {
+    addEvent(this.pickerEl, 'touchmove', (e) => {
+      e.preventDefault();
+    });
 
-		addEvent(this.confirmEl, 'click', () => {
-			this.hide();
+    addEvent(this.confirmEl, 'click', () => {
+      this.hide();
 
-			let changed = false;
-			for (let i = 0; i < this.data.length; i++) {
-				let index = this.wheels[i].getSelectedIndex();
-				this.selectedIndex[i] = index;
+      let changed = false;
+      for (let i = 0; i < this.data.length; i++) {
+        let index = this.wheels[i].getSelectedIndex();
+        this.selectedIndex[i] = index;
 
-				let value = null;
-				if (this.data[i].length) {
-					value = this.data[i][index].value;
-				}
-				if (this.selectedVal[i] !== value) {
-					changed = true;
-				}
-				this.selectedVal[i] = value;
-			}
+        let value = null;
+        if (this.data[i].length) {
+          value = this.data[i][index].value;
+        }
+        if (this.selectedVal[i] !== value) {
+          changed = true;
+        }
+        this.selectedVal[i] = value;
+      }
 
-			this.trigger('picker.select', this.selectedVal, this.selectedIndex);
+      this.trigger('picker.select', this.selectedVal, this.selectedIndex);
 
-			if (changed) {
-				this.trigger('picker.valuechange', this.selectedVal, this.selectedIndex);
-			}
-		});
+      if (changed) {
+        this.trigger('picker.valuechange', this.selectedVal, this.selectedIndex);
+      }
+    });
 
-		addEvent(this.cancelEl, 'click', () => {
-			this.hide();
-			this.trigger('picker.cancel');
-		});
-	}
+    addEvent(this.cancelEl, 'click', () => {
+      this.hide();
+      this.trigger('picker.cancel');
+    });
+  }
 
-	_createWheel(wheelEl, i) {
-		this.wheels[i] = new BScroll(wheelEl[i], {
-			wheel: true,
-			selectedIndex: this.selectedIndex[i]
-		});
-		((index) => {
-			this.wheels[index].on('scrollEnd', () => {
-				this.trigger('picker.change', index, this.wheels[index].getSelectedIndex());
-			});
-		})(i);
-		return this.wheels[i];
-	}
+  _createWheel(wheelEl, i) {
+    this.wheels[i] = new BScroll(wheelEl[i], {
+      wheel: true,
+      selectedIndex: this.selectedIndex[i]
+    });
+    ((index) => {
+      this.wheels[index].on('scrollEnd', () => {
+        this.trigger('picker.change', index, this.wheels[index].getSelectedIndex());
+      });
+    })(i);
+    return this.wheels[i];
+  }
 
-	show(next) {
-		this.pickerEl.style.display = 'block';
-		let showCls = this.options.showCls;
+  show(next) {
+    this.pickerEl.style.display = 'block';
+    let showCls = this.options.showCls;
 
-		window.setTimeout(() => {
-			addClass(this.maskEl, showCls);
-			addClass(this.panelEl, showCls);
+    window.setTimeout(() => {
+      addClass(this.maskEl, showCls);
+      addClass(this.panelEl, showCls);
 
-			if (!this.wheels) {
-				this.wheels = [];
-				for (let i = 0; i < this.data.length; i++) {
-					this._createWheel(this.wheelEl, i);
-				}
-			} else {
-				for (let i = 0; i < this.data.length; i++) {
-					this.wheels[i].enable();
-					this.wheels[i].wheelTo(this.selectedIndex[i]);
-				}
-			}
-			next && next();
-		}, 0);
-	}
+      if (!this.wheels) {
+        this.wheels = [];
+        for (let i = 0; i < this.data.length; i++) {
+          this._createWheel(this.wheelEl, i);
+        }
+      } else {
+        for (let i = 0; i < this.data.length; i++) {
+          this.wheels[i].enable();
+          this.wheels[i].wheelTo(this.selectedIndex[i]);
+        }
+      }
+      next && next();
+    }, 0);
+  }
 
-	hide() {
-		let showCls = this.options.showCls;
-		removeClass(this.maskEl, showCls);
-		removeClass(this.panelEl, showCls);
+  hide() {
+    let showCls = this.options.showCls;
+    removeClass(this.maskEl, showCls);
+    removeClass(this.panelEl, showCls);
 
-		window.setTimeout(() => {
-			this.pickerEl.style.display = 'none';
-			for (let i = 0; i < this.length; i++) {
-				this.wheels[i].disable();
-			}
-		}, 500);
-	}
+    window.setTimeout(() => {
+      this.pickerEl.style.display = 'none';
+      for (let i = 0; i < this.length; i++) {
+        this.wheels[i].disable();
+      }
+    }, 500);
+  }
 
-	refill(data, index) {
-		let scrollEl = this.scrollEl[index];
-		let wheel = this.wheels[index];
-		if (scrollEl && wheel) {
-			let oldData = this.data[index];
-			this.data[index] = data;
-			scrollEl.innerHTML = itemTemplate(data);
+  refillColumn(index, data) {
+    let scrollEl = this.scrollEl[index];
+    let wheel = this.wheels[index];
+    if (scrollEl && wheel) {
+      let oldData = this.data[index];
+      this.data[index] = data;
+      scrollEl.innerHTML = itemTemplate(data);
 
-			let selectedIndex = wheel.getSelectedIndex();
-			let dist = 0;
-			if (oldData.length) {
-				let oldValue = oldData[selectedIndex].value;
-				for (let i = 0; i < data.length; i++) {
-					if (data[i].value === oldValue) {
-						dist = i;
-						break;
-					}
-				}
-			}
-			this.selectedIndex[index] = dist;
-			wheel.refresh();
-			wheel.wheelTo(dist);
-			return dist;
-		}
-	};
+      let selectedIndex = wheel.getSelectedIndex();
+      let dist = 0;
+      if (oldData.length) {
+        let oldValue = oldData[selectedIndex].value;
+        for (let i = 0; i < data.length; i++) {
+          if (data[i].value === oldValue) {
+            dist = i;
+            break;
+          }
+        }
+      }
+      this.selectedIndex[index] = dist;
+      wheel.refresh();
+      wheel.wheelTo(dist);
+      return dist;
+    }
+  }
+
+  refill(datas) {
+    let ret = [];
+    if (!datas.length) {
+      return ret;
+    }
+    datas.forEach((data, index) => {
+      ret[index] = this.refillColumn(index, data);
+    });
+    return ret;
+  }
+
+  scrollColumn(index, dist) {
+    let wheel = this.wheels[index];
+    wheel.wheelTo(dist);
+  }
 }
