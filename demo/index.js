cd9523788b53a38fca26a20e91e5c36578b43131
neurotoxinvx <neurotoxin@vip.qq.com>
2016-11-12 14:45:13 +0800

    address demo

--- demo/index.js
+++ demo/index.js
@@ -1,101 +1,85 @@
-var nameEl = document.getElementById('name');
+var nameEl = document.getElementById('chose');
 
-var data1 = [
-	{
-		text: '小美',
-		value: 1
-	}, {
-		text: '猪猪',
-		value: 2
-	}
-];
+var first = []; /* 省，直辖市 */
+var second = []; /* 市 */
+var third = []; /* 镇 */
 
-var data2 = [
-	{
-		text: '张三',
-		value: 1
-	},
-	{
-		text: '李四',
-		value: 2
-	},
-	{
-		text: '王五',
-		value: 3
-	},
-	{
-		text: '赵六',
-		value: 4
-	},
-	{
-		text: '吴七',
-		value: 5
-	},
-	{
-		text: '陈八',
-		value: 6
-	},
-	{
-		text: '杜九',
-		value: 7
-	},
-	{
-		text: '黄十',
-		value: 8
-	},
-	{
-		text: '呵呵',
-		value: 9
-	},
-	{
-		text: '哈哈',
-		value: 10
-	},
-	{
-		text: '嘿嘿',
-		value: 11
-	},
-	{
-		text: '啦啦',
-		value: 12
-	}
-];
+var checked = [0, 0, 0]; /* 已选选项 */
 
-var data3 = [
-	{
-		text: '开心',
-		value: 1
-	}, {
-		text: '生气',
-		value: 2
-	},
-	{
-		text: '搞笑',
-		value: 3
-	}, {
-		text: '难过',
-		value: 4
-	}
-];
+city.forEach(function(item, index, arr){
+  var temp = new Object();
+  temp.text = item.name;
+  temp.value = index
+  first.push(temp)
+})
+
+city[0].sub.forEach(function(item, index, arr){
+  var temp = new Object();
+  temp.text = item.name;
+  temp.value = index
+  second.push(temp)
+})
 
 var picker = new Picker({
-	data: [data1, data2, data3],
-	selectedIndex: [0, 1, 2],
-	title: '我们都是小学生'
+	data: [first, second, third],
+	selectedIndex: [0, 0, 0],
+	title: '地址选择'
 });
 
 picker.on('picker.select', function (selectedVal, selectedIndex) {
-	nameEl.innerText = data1[selectedIndex[0]].text + ' ' + data2[selectedIndex[1]].text + ' ' + data3[selectedIndex[2]].text;
-})
+  var text1 = first[selectedIndex[0]].text;
+  var text2 = second[selectedIndex[1]].text;
+  var text3 = third[selectedIndex[2]] ? third[selectedIndex[2]].text : '';
+
+	nameEl.innerText = text1 + ' ' + text2 + ' ' + text3;
+});
 
 picker.on('picker.change', function (index, selectedIndex) {
-	console.log(index);
-	console.log(selectedIndex);
+  if (index === 0){
+    firstChange();
+  } else if (index === 1) {
+    secondChange()
+  }
+
+  function firstChange() {
+    second = [];
+    third = [];
+    checked[0] = selectedIndex;
+    var firstCity = city[selectedIndex]
+    firstCity.sub.forEach(function(item, index, arr){
+      var temp = new Object();
+      temp.text = item.name;
+      temp.value = index;
+      second.push(temp);
+    })
+    picker.refill(second, 1)
+    picker.refill(third, 2)
+  }
+
+  function secondChange() {
+    third = [];
+    checked[1] = selectedIndex;
+    var first_index = checked[0];
+    if (city[first_index].sub[selectedIndex].hasOwnProperty('sub')) {
+      var secondCity = city[first_index].sub[selectedIndex]
+      secondCity.sub.forEach(function(item, index, arr){
+        var temp = new Object();
+        temp.text = item.name;
+        temp.value = index;
+        third.push(temp);
+      })
+      picker.refill(third, 2)
+    } else {
+      third = [''];
+      checked[2] = 0;
+      picker.refill(third, 2)
+    }
+  }
+
 });
 
 picker.on('picker.valuechange', function (selectedVal, selectedIndex) {
-	console.log(selectedVal);
-	console.log(selectedIndex);
+
 });
 
 nameEl.addEventListener('click', function () {
