bd7a08f5ad05686bd735ba693c72643857ea95c6
wanghongyuneurotoxin <wanghongyuneurotoxin@didichuxing.com>
2017-02-27 17:59:11 +0800

    fixed

--- demo/index.js
+++ demo/index.js
@@ -4,6 +4,8 @@ var first = []; /* 省，直辖市 */
 var second = []; /* 市 */
 var third = []; /* 镇 */
 
+var selectedIndex = [6, 2, 2]; /* 默认选中的地区 */
+
 var checked = [0, 0, 0]; /* 已选选项 */
 
 function creatList(obj, list){
@@ -17,21 +19,21 @@ function creatList(obj, list){
 
 creatList(city, first);
 
-if (city[0].hasOwnProperty('sub')) {
-  creatList(city[0].sub, second);
+if (city[selectedIndex[0]].hasOwnProperty('sub')) {
+  creatList(city[selectedIndex[0]].sub, second);
 } else {
   second = [{text: '', value: 0}];
 }
 
-if (city[0].sub[0].hasOwnProperty('sub')) {
-  creatList(city[0].sub[0].sub, third);
+if (city[selectedIndex[0]].sub[selectedIndex[1]].hasOwnProperty('sub')) {
+  creatList(city[selectedIndex[0]].sub[selectedIndex[1]].sub, third);
 } else {
   third = [{text: '', value: 0}];
 }
 
 var picker = new Picker({
 	data: [first, second, third],
-	selectedIndex: [0, 0, 0],
+  selectedIndex: selectedIndex,
 	title: '地址选择'
 });
 
